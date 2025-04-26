64681d7641107a95f255e9da41c4dc5677cce8d5
Saeed Anwar <saeed@finehomeint.com>
2025-04-26 09:50:45 +0400

    git bhut CTION

--- app/database.py
+++ app/database.py
@@ -1,10 +1,14 @@
-
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@db:5432/postgres")
+# Use Render-provided DATABASE_URL if it exists — fallback only if running locally
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    # fallback for local dev only
+    DATABASE_URL = "postgresql://postgres:1234@db:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
